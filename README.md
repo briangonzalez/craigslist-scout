d11780e2990285410708eb53814c6daabad7b246
Brian Gonzalez <me@briangonzalez.org>
2014-09-19 08:28:28 -0700

    second initial commit

--- README.md
+++ README.md
@@ -1,5 +1,39 @@
-craigslist-hound
+craigslist-scout
 ----------------
 
+A dead simple, command line tool to query multiple Craigslist cities (subdomains) and email yourself the results.
 
-./index.js --interval 0.5 --cities slo,losangeles,sandiego,fresno,bakersfield,orangecounty,inlandempire --query "cto?maxAsk=10000&query=tacoma%20prerunner&sort=date" --recipients "me@briangonzalez.org,largonzal@gmail.com" --sender 'me@briangonzalez.org:123456'
+Usage
+-----
+
+````
+  Usage: cl-scout [options]
+
+  Options:
+
+    -c, --cities [type]       Comma delimited list of cities (corresponds to craigslist subdomains)
+    -q, --query [type]        Search query: https://losangeles.craigslist.org/search/<<search query>>
+    -i, --interval [type]     Query every "n" minutes
+    -s, --sender [type]       Sender credentials in the form of sender@gmail.com:my_password; set to false to disable email
+    -r, --recipients [items]  Comma delimited list of recipients
+    -d, --database [type]     LowDB database to store results in
+    -e, --seed                Seed your DB before scouting.
+````
+
+An Example Query
+----------------
+
+To search Craigslist in LA, SLO, San Diego, Fresno, Bako, OC, Inland Empire, and Santa Barbara ever 0.5 minutes for Tacomas and send new results to `her@briangonzalez.org` and `him@briangonzalez.org` from `me@briangonzalez.org`:
+
+```
+cl-scout  --interval 0.5 \
+          --cities slo,losangeles,sandiego,fresno,bakersfield,orangecounty,inlandempire,santabarbara \
+          --query "cto?maxAsk=10000&query=tacoma&sort=date" \
+          --recipients "her@briangonzalez.org,him@briangonzalez.org" \
+          --sender 'me@briangonzalez.org:foobar'
+```
+
+## Author
+| ![twitter/brianmgonzalez](http://gravatar.com/avatar/f6363fe1d9aadb1c3f07ba7867f0e854?s=70](http://twitter.com/brianmgonzalez "Follow @brianmgonzalez on Twitter") |
+|---|
+| [Brian Gonzalez](http://briangonzalez.org) |
--- index.js
+++ index.js
@@ -3,35 +3,37 @@
 var program = require('commander');
 var app = require('./package.json');
 var urlBuilder = require('./lib/url-builder');
-var hound = require('./lib/cl-hound');
+var scout = require('./lib/scout');
 var Secretary = require('./lib/secretary');
 
 program
   .version( app.version )
   .usage('[options]')
-  .option('-c, --cities [type]',              'Comma delimited list of cities (corresponds to craigslist subdomains)', 'losangeles,fresno')
-  .option('-q, --query [type]',               'Search query: https://slo.craigslist.org/search/<<search query>>',
-                                              'cto?maxAsk=10000&query=tacoma%20prerunner&sort=rel')
+  .option('-c, --cities [type]',              'Comma delimited list of cities (corresponds to craigslist subdomains)', 'losangeles,ventura')
+  .option('-q, --query [type]',               'Search query: https://losangeles.craigslist.org/search/<<search query>>',
+                                              'cto?maxAsk=10000&query=tacoma&sort=rel')
   .option('-i, --interval [type]',            'Query every "n" minutes', 1)
-  .option('-s, --sender [type]',              'Sender credentials', 'bar@gmail.com:123456')
-  .option('-r, --recipients [type]',          'Comma delimited list of recipients', 'bar@gmail.com')
+  .option('-s, --sender [type]',              'Sender credentials in the form of sender@gmail.com:my_password; set to false to disable email', 'false')
+  .option('-r, --recipients [items]',         'Comma delimited list of recipients', 'bar@gmail.com,foo@baz.com')
   .option('-d, --database [type]',            'LowDB database to store results in', 'db.json')
-  .option('-e, --seed',                       'Seed your DB before beginning.')
+  .option('-e, --seed',                       'Seed your DB before scouting.')
   .parse(process.argv);
 
 //
 // 1. Get URLs for all cities.
-// 2. Start ping the URLs
-// 3. Save URLs to DB via secretary.
+// 2. Start pinging the URLs
+// 3. Save URLs to DB.
+// 4. Email new results.
 //
 var urls = urlBuilder.getURLs(program.cities, program.query);
 
 var secretary = new Secretary({
     database: program.database,
+    seed: program.seed,
+    recipients: program.recipients,
     sender: program.sender,
-    seed: program.seed
   });
 
-secretary.listen(hound);
+secretary.listen(scout);
 
-hound.start({ urls: urls, interval: program.interval, seed: program.seed })
+scout.start({ urls: urls, interval: program.interval, seed: program.seed })
